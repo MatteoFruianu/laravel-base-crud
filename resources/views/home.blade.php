c95e0fae1058bdf04dc8d6e5d1fb7a146542faa4
Matteo Fruianu <matteofruianu@gmail.com>
2021-01-21 16:57:03 +0100

    Crud id

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container mb-5">
+        <h1>School Home Page Here</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, nam? Recusandae voluptatum, minus ex iste accusantium incidunt. Sapiente illo vero quisquam architecto aliquid distinctio impedit, nemo consectetur nobis ipsam dignissimos.</p>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// HOMEPAGE
+Route::get('/', 'HomeController@index')->name('homepage');
+
+
+// CLASSROOMS RESOURCES PAGES (CRUD)
+
+Route::resource('classrooms', 'ClassroomController'); //in questo Laravel crea tutti gli endpoint necessari per fare CRUD
